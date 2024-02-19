058888ef937e92dd0b9fe17d114d932877fd39ba
Misskarina <karinakumari907ce@gmail.com>
2024-02-19 14:12:04 +0530

    API Testing in postman

---
+++ src/test/java/com/Demo/APItestingPostman/ApItestingPostmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Demo.APItestingPostman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApItestingPostmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
